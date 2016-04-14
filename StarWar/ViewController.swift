28c39a2798e8bfe3197e1aa481b12e55dd4b5a79
Betterteng <betterteng@gmail.com>
2016-04-14 18:30:18 +1000

    Initial Commit

---
+++ StarWar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWar
+//
+//  Created by 滕施男 on 14/04/2016.
+//  Copyright © 2016 TENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
